eeb0e30fbb5da17cb9be9d32aa9651211d82b036
Peu <peustratt@gmail.com>
2022-05-25 19:08:40 -0300

    on load scroll page to most recent messasges

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -3,9 +3,12 @@ function joinRoom(roomName) {
         document.querySelector('.room-header__users').innerHTML = `${newNumberOfUsers}<span class="glyphicon glyphicon-user"></span>`;
     });
     nsSocket.on('historyCatchUp', (messages) => {
+        const messagesUl = document.getElementById('messages')
+        messagesUl.innerHTML = '';
         messages.forEach(message => {
             console.log(message)
-            document.getElementById('messages').innerHTML += buildHTML(message);
+            messagesUl.innerHTML += buildHTML(message);
         })
+        messagesUl.scrollTo(0, messagesUl.scrollHeight);
     })
 }
